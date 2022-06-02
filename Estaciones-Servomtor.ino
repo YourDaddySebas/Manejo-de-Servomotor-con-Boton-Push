32ff7be23759866d0bed0496f15aea92e0230ff6
YourDaddySebas <96026758+YourDaddySebas@users.noreply.github.com>
2022-06-02 17:00:54 -0600

    Update Estaciones-Servomtor.ino

--- Estaciones-Servomtor.ino
+++ Estaciones-Servomtor.ino
@@ -1,37 +1,35 @@
-/* Sebastian camacho A.*/
-/*código para practica #2 - mantenimiento
+/* © Sebastian camacho A.*/
+/*código para practica #2 - mantenimiento mecatronico
    hacer "Estaciones con Servo y boton 
   ---- 4 Estaciones--- */
 
-#include <Servo.h>
-Servo servoMotor;
+#include <Servo.h> //Incluimos la libreria de Servo para poder controlar nuestro Servomotor
+Servo servoMotor; //Declaramos nuestro Servomotor
 //Declaraciones
-int boton = 9;
-int estacion = 0;
-int conta = 0;
-long time = 0;
-int rebote = 200;
+int boton = 9; //Declaramos el pin del boton que usaremos (En mi caso fue el pin 9)
+int estacion = 0; //Declaramos la variable estacion para usarla mas tarde
+int conta = 0; //Declaramos la variable "conta" para usarla como sumatoria
+long time = 0; //Declaramos "time" para usarlo en el anti-rebote
+int rebote = 200; //Declaramos los ms que va a tener nuestro rebote
+
 void setup()
 {
-  servoMotor.attach(A0);
-  pinMode(boton, INPUT);
-  servoMotor.write(0);
-  Serial.begin(9600);
+  servoMotor.attach(A0); //Declaramos donde esta conectado nuestro Servomotor (E mi caso A0)
+  pinMode(boton, INPUT); //Declaramos el boton de manera INPUT
+  servoMotor.write(0); //Colocamos el Servo en 0°, al iniciar el programa
+  Serial.begin(9600); //Usaremos serial begin para leer la variable "conta", puedes quitarla o modicar el serial.print
 }
 
 void loop()
 {
   
-  if( (digitalRead(boton) == 1) && (millis() - time > rebote) ){
+  if( (digitalRead(boton) == 1) && (millis() - time > rebote) ){ //Declaramos si el boton fue seleccionado haz esto↓↓
     time = millis();
-    conta++;
-    Serial.println(conta);
+    conta++; //conta++ nos ayuda a hacer la sumatoria cda que pulsemos el boton
+    Serial.println(conta); //Aqui le decimos a Serial lo que queremos que muestre en el monitor serial, en este caso "conta"
   }
-  /*
-  if(conta == 1){
-    estacion = 1;
-  }*/
-  switch(conta){
+  
+  switch(conta){ //usamos la variable switch para mandar las funciones correspondientes segun el pulso que va
     case 0:
     estacion = 0;
     break;
@@ -47,15 +45,15 @@ void loop()
     case 4:
     estacion = 4;
     break;
-    case 5:
+    case 5: //como solo tendremos 4 estaciones o grados, al quinto pulso mandamos la variable "conta" de nuevo al inicio "0"
     conta = 0;
     break;
     
     }
   
-  switch(estacion){
+  switch(estacion){ //con switch posicionaremos nuestro servo segun el numero de estacion que lleve el contador
     case 0:
-    servoMotor.write(0);
+    servoMotor.write(0); //.write nos sirve para indicarle al servomotor que grado° debe posicionarse (cambia los valores por los tuyos)
     break;
     case 1:
     servoMotor.write(40);
@@ -71,3 +69,5 @@ void loop()
     break;
   }
 }
+/*© Sebastián camacho - 2022 - 2023 - borrar de ser necesario*/
+
